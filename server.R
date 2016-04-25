85e6b3cd4d983ca9c2e2492d588c9b6e1eb9ec42
Eugene Brown <efbbrown@gmail.com>
2016-04-25 19:08:52 +1000

    Added g3, removed redundant server.R lines

--- server.R
+++ server.R
@@ -1,15 +1,3 @@
-pacman::p_load(shiny, jsonlite)
-
-source("R/make_data.R")
-
 shinyServer(function(input, output, session) {
-  
-  observe({
-    
-    data <- toJSON(data)
-    
-    session$sendCustomMessage(type = "data", data)
-    
-  })
   
 })
